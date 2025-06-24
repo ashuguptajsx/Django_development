e14ab80f119c70096e3398cbd2962d18f579abb4
ashuguptajsx <ashugupta1403@gmail.com>
2025-06-24 23:15:52 +0530

    commit

--- pythondev/tweet/views.py
+++ pythondev/tweet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required 
 
 # Create your views here.
 
@@ -16,6 +17,8 @@ def tweet_list(request):
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
 
+
+@login_required
 def tweet_create(request):
     if request.method == 'POST':
      form =  TweetForm(request.POST, request.FILES)
@@ -29,7 +32,7 @@ def tweet_create(request):
     return render(request, 'tweet_form.html', {'form': form})
 
 
-
+@login_required
 def tweet_edit(request, tweet_id):
    tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
    if request.method == 'POST':
@@ -50,3 +53,7 @@ def tweet_delete(request, tweet_id):
        tweet.delete()
        return redirect('tweet_list')
     return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
+
+
+def register(request):
+     return render(request, 'registration/register.html', {'form': form})
